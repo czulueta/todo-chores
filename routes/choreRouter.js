f6dd6eb7749f06ec0615406a79596abba0bb1a48
czulueta <https://github.com/czulueta/weed.git>
2021-01-12 19:42:56 -0800

    added delete and update routes to chorerouter

--- routes/choreRouter.js
+++ routes/choreRouter.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const choreRouter = express.Router()
 const Chore = require("../models/chore.js")
-
+// get all
 choreRouter.get("/", (req, res, next) => {
     Chore.find((err, chores) => {
         if(err){
@@ -11,6 +11,7 @@ choreRouter.get("/", (req, res, next) => {
         return res.status(200).send(chores)
     })
 })
+// post new chores
 choreRouter.post("/", (req, res, next) => {
     const newChore = new Chore(req.body)
     newChore.save((err, savedChore) => {
@@ -20,5 +21,30 @@ choreRouter.post("/", (req, res, next) => {
         }
         return res.status(201).send(savedChore)
     })
+})
+// delete chores
+choreRouter.delete("/choreId", (req, res, next) => {
+    Chore.findOneAndDelete( {_id: req.params.choreId}, (err, deletedChore) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(`Successfully deleted ${deletedChore.choreName} from the list of chores`)
+    })
+})
+//updated chore
+choreRouter.put("/choreId", (req, res, next) => {
+    Chore.findOneAndUpdate(
+        { _id: req.params.choreId },
+        req.body,
+        { new: true },
+        (err, updatedChore) => {
+            if(err){
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(updatedChore)
+        }
+    )
 })
 module.exports = choreRouter
